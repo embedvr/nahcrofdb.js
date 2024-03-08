90ebe8d8c503e32dbe4646625ca075533ee92650
embed <brydon@helium.email>
2024-03-08 16:56:41 -0500

    [+] getKeys functin

--- index.js
+++ index.js
@@ -1,174 +1,204 @@
 import { fetch } from "@whatwg-node/fetch";
 
 class Client {
-  /**
-   * @param {string} username
-   * @param {string} apiKey
-   */
-  constructor(username, apiKey) {
-    /** @private */
-    this.username = username;
-    /** @private */
-    this.apiKey = apiKey;
-  }
+	/**
+	 * @param {string} username
+	 * @param {string} apiKey
+	 */
+	constructor(username, apiKey) {
+		/** @private */
+		this.username = username;
+		/** @private */
+		this.apiKey = apiKey;
+	}
 
-  /**
-   * Returns value of a single key in the database
-   * @param {string} key
-   * @returns {string}
-   */
-  async get(key) {
-    try {
-      const url = new URL("https://database.nahcrof.com/getKey");
-      url.searchParams.set("location", this.username);
-      url.searchParams.set("token", this.apiKey);
-      url.searchParams.set("keyname", key);
-      const res = await fetch(url);
+	/**
+	 * Returns value of a single key in the database
+	 * @param {string} key
+	 * @returns {string}
+	 */
+	async get(key) {
+		try {
+			const url = new URL("https://database.nahcrof.com/getKey");
+			url.searchParams.set("location", this.username);
+			url.searchParams.set("token", this.apiKey);
+			url.searchParams.set("keyname", key);
+			const res = await fetch(url);
 
-      if (!res.ok) {
-        throw new Error(await res.text());
-      }
+			if (!res.ok) {
+				throw new Error(await res.text());
+			}
 
-      const data = await res.json();
+			const data = await res.json();
 
-      if (data.keycontent === "Key not found") {
-        throw new Error("Key does not exist");
-      } else {
-        return data.keycontent;
-      }
-    } catch (err) {
-      throw new Error(err);
-    }
-  }
+			if (data.keycontent === "Key not found") {
+				throw new Error("Key does not exist");
+			} else {
+				return data.keycontent;
+			}
+		} catch (err) {
+			throw new Error(err);
+		}
+	}
 
-  /**
-   * Returns all keys in the database
-   * @returns {object}
-   */
-  async getAll() {
-    try {
-      const url = new URL("https://database.nahcrof.com/getAll");
-      url.searchParams.set("location", this.username);
-      url.searchParams.set("token", this.apiKey);
+	/**
+	 * Returns the specitifed keys in the database
+	 * @param {...string} keyNames
+	 * @returns {Record<string, string>}
+	 */
+	async getKeys(...keyNames) {
+		try {
+			const url = new URL("https://database.nahcrof.com/getKeys");
+			url.searchParams.set("location", this.username);
+			url.searchParams.set("token", this.apiKey);
+			url.searchParams.set("keynamenum", keyNames.length);
 
-      const res = await fetch(url);
+			const result = keyNames.map((key, index) => {
+				url.searchParams.set(`key_${index}`, key);
+			});
 
-      if (!res.ok) {
-        throw new Error(await res.text());
-      }
+			const res = await fetch(url);
 
-      const data = await res.json();
+			if (!res.ok) {
+				throw new Error(await res.text());
+			}
 
-      return data;
-    } catch (err) {
-      throw new Error(err);
-    }
-  }
+			const data = await res.json();
 
-  /**
-   * Creates a new key
-   * @param {string} key
-   * @param {string} value
-   * @returns {boolean}
-   */
-  async create(key, value) {
-    try {
-      const res = await fetch(`https://database.nahcrof.com/makeKey`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          location: this.username,
-          token: this.apiKey,
-          keyname: key,
-          keycontent: value,
-        }),
-      });
+			return data;
+		} catch (err) {
+			throw new Error(err);
+		}
+	}
 
-      if (!res.ok) {
-        throw new Error(await res.text());
-      }
+	/**
+	 * Returns all keys in the database
+	 * @returns {Record<string, string>}
+	 */
+	async getAll() {
+		try {
+			const url = new URL("https://database.nahcrof.com/getAll");
+			url.searchParams.set("location", this.username);
+			url.searchParams.set("token", this.apiKey);
 
-      const data = await res.text();
+			const res = await fetch(url);
 
-      if (data == "success") {
-        return true;
-      } else {
-        return false;
-      }
-    } catch (err) {
-      throw new Error(err);
-    }
-  }
+			if (!res.ok) {
+				throw new Error(await res.text());
+			}
 
-  /**
-   * Deletes a key from the database
-   * @param {string} key
-   * @returns {boolean}
-   */
-  async delete(key) {
-    try {
-      const res = await fetch(`https://database.nahcrof.com/delKey`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          location: this.username,
-          token: this.apiKey,
-          keyname: key,
-        }),
-      });
+			const data = await res.json();
 
-      if (!res.ok) {
-        throw new Error(await res.text());
-      }
+			return data;
+		} catch (err) {
+			throw new Error(err);
+		}
+	}
 
-      const data = await res.text();
+	/**
+	 * Creates a new key
+	 * @param {string} key
+	 * @param {string} value
+	 * @returns {boolean}
+	 */
+	async create(key, value) {
+		try {
+			const res = await fetch(`https://database.nahcrof.com/makeKey`, {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify({
+					location: this.username,
+					token: this.apiKey,
+					keyname: key,
+					keycontent: value,
+				}),
+			});
 
-      if (data == "success") {
-        return true;
-      } else {
-        return false;
-      }
-    } catch (err) {
-      throw new Error(err);
-    }
-  }
+			if (!res.ok) {
+				throw new Error(await res.text());
+			}
 
-  /**
-   * Resets the database
-   * @returns {boolean}
-   */
-  async reset() {
-    try {
-      const res = await fetch(`https://database.nahcrof.com/resetDB`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          location: this.username,
-          token: this.apiKey,
-        }),
-      });
+			const data = await res.text();
 
-      if (!res.ok) {
-        throw new Error(await res.text());
-      }
+			if (data == "success") {
+				return true;
+			} else {
+				return false;
+			}
+		} catch (err) {
+			throw new Error(err);
+		}
+	}
 
-      const data = await res.text();
+	/**
+	 * Deletes a key from the database
+	 * @param {string} key
+	 * @returns {boolean}
+	 */
+	async delete(key) {
+		try {
+			const res = await fetch(`https://database.nahcrof.com/delKey`, {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify({
+					location: this.username,
+					token: this.apiKey,
+					keyname: key,
+				}),
+			});
 
-      if (data == "success") {
-        return true;
-      } else {
-        return false;
-      }
-    } catch (err) {
-      throw new Error(err);
-    }
-  }
+			if (!res.ok) {
+				throw new Error(await res.text());
+			}
+
+			const data = await res.text();
+
+			if (data == "success") {
+				return true;
+			} else {
+				return false;
+			}
+		} catch (err) {
+			throw new Error(err);
+		}
+	}
+
+	/**
+	 * Resets the database
+	 * @returns {boolean}
+	 */
+	async reset() {
+		try {
+			const res = await fetch(`https://database.nahcrof.com/resetDB`, {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify({
+					location: this.username,
+					token: this.apiKey,
+				}),
+			});
+
+			if (!res.ok) {
+				throw new Error(await res.text());
+			}
+
+			const data = await res.text();
+
+			if (data == "success") {
+				return true;
+			} else {
+				return false;
+			}
+		} catch (err) {
+			throw new Error(err);
+		}
+	}
 }
 
 export default Client;
